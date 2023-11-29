983f917a3d0460436c691c7998248310da7c26bd
Rafael Cruz <rafaelcruz_net@outlook.com>
2023-11-29 07:21:22 -0300

    Desenvolvimento

---
+++ SpotifyLike.Application/Conta/UsuarioService.cs
@@ -0,0 +1,21 @@
+﻿using SpotifyLike.Application.Conta.Dto;
+using SpotifyLike.Domain.Conta.Agreggates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotifyLike.Application.Conta
+{
+    public class UsuarioService
+    {
+        public CriarUsuarioDTO CriarUsuario(CriarUsuarioDTO dto)
+        {
+            //Todo:
+            Cartao
+
+
+        }
+    }
+}
